cefe18fcd03299d6a60ba94809cab1390bc7bf5f
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2025-03-09 12:32:06 -0600

    sss

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
 require("dotenv").config();
-const cors = require("cors");
+// const cors = require("cors");
 const express = require("express");
-const cookieParser = require("cookie-parser");
-const rutasLogin = require("./login/login.routes");
-const rutasS3 = require("./S3/s3.routes");
-const swaggerUI = require("swagger-ui-express");
-const swaggerJsDoc = require("swagger-jsdoc");
+// const cookieParser = require("cookie-parser");
+// const rutasLogin = require("./login/login.routes");
+// const rutasS3 = require("./S3/s3.routes");
+// const swaggerUI = require("swagger-ui-express");
+// const swaggerJsDoc = require("swagger-jsdoc");
 
 const app = express();
 const PORT = process.env.port || 3000;
 
 // login using jwt
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "API de ejemplo",
-      version: "1.0.0",
-      description: "Documentación generada con Swagger",
-    },
-    servers: [
-      {
-        url: "http://localhost:3000",
-      },
-    ],
-  },
-  apis: ["login/login.routes.js"], // Ruta donde están definidas tus rutas
-};
+// const options = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "API de ejemplo",
+//       version: "1.0.0",
+//       description: "Documentación generada con Swagger",
+//     },
+//     servers: [
+//       {
+//         url: "http://localhost:3000",
+//       },
+//     ],
+//   },
+//   apis: ["login/login.routes.js"], // Ruta donde están definidas tus rutas
+// };
 
-const specs = swaggerJsDoc(options);
+// const specs = swaggerJsDoc(options);
 
 // Middleware para Swagger UI
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://main.d19eu3ca4s0hn8.amplifyapp.com/", // Allow only your frontend
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
-    credentials: true, // Allow cookies if needed
-  })
-);
-app.use(cookieParser());
+// app.use(
+//   cors({
+//     origin: "https://main.d19eu3ca4s0hn8.amplifyapp.com/", // Allow only your frontend
+//     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+//     credentials: true, // Allow cookies if needed
+//   })
+// );
+// app.use(cookieParser());
 
 app.get("/", async (req, res) => {
   console.log("ruta /");
-  res.status(201).json({ message: "Pene" });
+  res.status(201).json({ message: "Hola" });
 });
 
-app.use("/api", rutasLogin);
-app.use("/s3", rutasS3);
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+// app.use("/api", rutasLogin);
+// app.use("/s3", rutasS3);
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 //http://localhost:3000/s3/upload
 
