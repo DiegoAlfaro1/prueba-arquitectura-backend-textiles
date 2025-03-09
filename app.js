5e09895783bf040938276259d0fbcfcaa0c39180
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2025-03-09 15:37:17 -0600

    rollback

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const rutasLogin = require("./login/login.routes");
 const rutasS3 = require("./S3/s3.routes");
-const rutasAutorizacion = require("./util/authorize.routes");
 const swaggerUI = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
 
@@ -37,15 +36,11 @@ const specs = swaggerJsDoc(options);
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "https://main.d19eu3ca4s0hn8.amplifyapp.com", // Allow your production frontend
-      "http://localhost:5174", // Allow your local frontend for development
-    ],
+    origin: "*", // Allow only your frontend
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
     credentials: true, // Allow cookies if needed
   })
 );
-
 app.use(cookieParser());
 
 app.get("/", async (req, res) => {
@@ -55,7 +50,6 @@ app.get("/", async (req, res) => {
 app.use("/api", rutasLogin);
 app.use("/s3", rutasS3);
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-app.use("/auth", rutasAutorizacion);
 
 //http://localhost:3000/s3/upload
 
--- util/authorize.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const authorizeToken = require("./authorizeToken");
-const router = express.Router();
-
-router.get("/validate-token", authorizeToken, (req, res) => {
-  res.json({ user: req.user });
-});
