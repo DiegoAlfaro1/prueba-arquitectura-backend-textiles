91fe82db144380973cd02dc0be1c45475124cbb3
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2025-03-10 17:33:39 -0600

    intentar subir cositas para deploy un backend de staging

---
+++ login/Controllers/login.controller.js
@@ -0,0 +1,71 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const userRepository = require("../Data/userRepository");
+
+const JWT_SECRET = "mysecretkey123";
+
+exports.register = async (req, res) => {
+  const { email, password, name } = req.body;
+
+  if (!email || !password) {
+    return res.status(401).json({ message: "Se ocupa el mail y contra" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    await userRepository.createUser({ email, hashedPassword, name });
+    res.status(201).json({ message: "Registrado exitosamente" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password, name } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Se necesita email y contraseña" });
+  }
+
+  try {
+    const user = await userRepository.getUserByEmail(email, name);
+
+    if (!user) {
+      return res.status(401).json({ message: "Usuario no encontrado" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.hashedPassword);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Contraseña incorrecta" });
+    }
+
+    const token = jwt.sign({ email: user.email }, JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    });
+
+    res.json({ message: "Inicio de sesion exitoso" });
+  } catch (error) {
+    res.status(500).json({ message: "Error al iniciar sesión", error });
+  }
+};
+
+exports.getUsers = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await userRepository.getUserById(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Error al obtener usuario" });
+  }
+};
---
+++ login/Data/userRepository.js
@@ -0,0 +1,37 @@
+// data/userRepository.js
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+} = require("@aws-sdk/lib-dynamodb");
+
+const dynamoClient = new DynamoDBClient({ region: "us-east-1" });
+const db = DynamoDBDocumentClient.from(dynamoClient);
+const TABLE_NAME = "Users";
+
+exports.createUser = async (user) => {
+  const command = new PutCommand({
+    TableName: TABLE_NAME,
+    Item: user,
+  });
+  return db.send(command);
+};
+
+exports.getUserByEmail = async (email, name) => {
+  const command = new GetCommand({
+    TableName: TABLE_NAME,
+    Key: { email, name },
+  });
+  const response = await db.send(command);
+  return response.Item;
+};
+
+exports.getUserById = async (id) => {
+  const command = new GetCommand({
+    TableName: TABLE_NAME,
+    Key: { id },
+  });
+  const response = await db.send(command);
+  return response.Item;
+};
