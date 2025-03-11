92a75915e3b5f1a1696ecc0d82cb83f24a520df7
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2025-03-11 17:40:04 -0600

    prueba para hacer deploy en staging al igual que en production

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
+const envFile = `.env.${process.env.NODE_ENV || "staging"}`; // Defaults to 'development' if NODE_ENV is not set
+dotenv.config({ path: envFile });
+
 const cors = require("cors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,24 @@
 module.exports = {
   apps: [
     {
-      name: "production", // Name of your app
+      name: "app-production", // Name of your app
       script: "./app.js", // Path to your entry file (e.g., app.js, server.js, etc.)
       instances: "max", // Run maximum instances of your app based on available CPU cores
       exec_mode: "cluster", // Use cluster mode for load balancing (optional)
       watch: false, // Disable file watching (set to true if you want PM2 to restart on file changes)
       env: {
         NODE_ENV: "production", // Set the environment variable to "production"
+        PORT: 3000,
       },
-      env_production: {
-        NODE_ENV: "production", // Set environment variable for production explicitly (optional)
+    },
+    {
+      name: "app-staging", // Name of your app
+      script: "./app.js", // Path to your entry file (e.g., app.js, server.js, etc.)
+      instances: "max", // Run maximum instances of your app based on available CPU cores
+      exec_mode: "cluster", // Use cluster mode for load balancing (optional)
+      watch: false, // Disable file watching (set to true if you want PM2 to restart on file changes)
+      env: {
+        NODE_ENV: "staging", // Set environment variable for production explicitly (optional)
         PORT: 4000, // Define the port your app runs on (optional)
       },
     },
