cbc9fe106b482bc614dc7d456d2b78c5b98c0644
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2025-03-27 11:55:00 -0600

    arreglar response

--- util/checkHeader.js
+++ util/checkHeader.js
@@ -2,7 +2,7 @@ module.exports = (headerName, errorMessage = "Api key invalida") => {
   return (req, res, next) => {
     const headerValue = req.get(headerName);
     if (!headerValue) {
-      return req.status(400).json({ message: errorMessage });
+      return res.status(400).json({ message: errorMessage });
     }
     next();
   };
